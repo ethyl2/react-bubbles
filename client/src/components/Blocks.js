6bc2b25aa39d0beb0400f6e177b5f3f58d4885d4
Heather Nuffer <zzzuzz@gmail.com>
2019-12-20 11:47:04 -0700

    Blocks page has 2 visualizations

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from "../utils/axiosWithAuth";
-import { Pack } from "@potion/layout";
+import { Pack, Partition } from "@potion/layout";
 import { Svg, Rect } from "@potion/element";
 
 const Blocks = props => {
@@ -31,45 +31,85 @@ const Blocks = props => {
 
     return (
         <div>
-            <h2>Blocks will go here</h2>
-        <div className="bubble-wrap">
-            <p>blocks</p>
-            <Svg width={400} height={400}>
+            <h2>blocks</h2>
+            <div className='blocks-display'>
+                <div>
+                    <p>pack</p>
+                    <Svg width={400} height={400}>
 
-                <Pack
-                data={{
-                    children: blocksData
-                }}
-                sum={datum => datum.value}
-                size={[400, 400]}
-                includeRoot={false}
-                nodeEnter={d => ({ ...d, r: 0 })}
-                animate
-                >
-                {nodes =>
-                    nodes
-                    .map(({ x, y, r, key }, i) => {
-                        if (i < colorList.length) {
-                        return (
-                            <Rect
-                            key={key}
-                            x={x}
-                            y={y}
-                            width={r}
-                            height={r}
-                            fill={colorList[i].code.hex}
-                            />
-                        );
+                        <Pack
+                        data={{
+                            children: blocksData
+                        }}
+                        sum={datum => datum.value}
+                        size={[400, 400]}
+                        includeRoot={false}
+                        nodeEnter={d => ({ ...d, r: 0 })}
+                        animate
+                        >
+                        {nodes =>
+                            nodes
+                            .map(({ x, y, r, key }, i) => {
+                                if (i < colorList.length) {
+                                return (
+                                    <Rect
+                                    key={key}
+                                    x={x}
+                                    y={y}
+                                    width={r}
+                                    height={r}
+                                    fill={colorList[i].code.hex}
+                                    />
+                                );
+                                }
+                                return null;
+                            })
+                            .filter(v => v)
                         }
-                        return null;
-                    })
-                    .filter(v => v)
-                }
-                </Pack>
+                        </Pack>
+                    </Svg>
+                </div>
+                
 
-            </Svg>
+                <div>
+                    <p>partition</p>
+                    <Svg width={400} height={200}>
+
+                        <Partition
+                        data={{
+                            children: blocksData
+                        }}
+                        sum={datum => datum.value}
+                        size={[400, 200]}
+                        includeRoot={false}
+                        nodeEnter={d => ({ ...d, r: 0 })}
+                        animate
+                        >
+                        {nodes =>
+                            nodes
+                            .map(({ x0, y0, x1, y1, key }, i) => {
+                                if (i < colorList.length) {
+                                return (
+                                    <Rect
+                                    key={key}
+                                    x={x0}
+                                    y={y0}
+                                    width={x1- x0}
+                                    height={y1 -y0}
+                                    fill={colorList[i].code.hex}
+                                    />
+                                );
+                                }
+                                return null;
+                            })
+                            .filter(v => v)
+                        }
+                        </Partition>
+
+                    </Svg>
+                </div>
+            </div>            
         </div>
-    </div>
     );    
     
 }
